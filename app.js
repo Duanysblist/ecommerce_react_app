e2fd4a839cc2992695f5824a45a489f1ae62e55c
Daniel Duany <dduany919@gmail.com>
2019-06-10 23:15:34 -0400

    Started server and used nodemon to update

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 require('dotenv').config();
 
 app.get('/', (req, res) => {
-  res.send('hello from node');
+  res.send('hello from node updated');
 });
 
 const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
